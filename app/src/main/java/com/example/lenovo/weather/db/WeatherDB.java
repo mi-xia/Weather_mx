13abe39abc737f839d7cb30faea3805b2755f392
mi-xia <18363994558@163.com>
2016-05-13 17:39:00 +0800

    增加数据库帮助类，各项表项和实体，第一阶段

---
+++ app/src/main/java/com/example/lenovo/weather/db/WeatherDB.java
@@ -0,0 +1,142 @@
+package com.example.lenovo.weather.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.lenovo.weather.model.City;
+import com.example.lenovo.weather.model.County;
+import com.example.lenovo.weather.model.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.CheckedOutputStream;
+
+/**
+ * Created by lenovo on 2016/5/13.
+ */
+public class WeatherDB {
+
+    /**
+     * 数据库名
+     */
+    public static final String DB_NAME = "weather";
+
+    public static final int VERSION = 1;
+
+    private SQLiteDatabase db;
+
+    private static WeatherDB weatherDB;
+
+    private WeatherDB(Context context){
+        WeatherOpenHelper dbhelper = new WeatherOpenHelper(context, DB_NAME, null, VERSION);
+        db = dbhelper.getWritableDatabase();
+    }
+
+    /**
+     获取WeatherDB实例
+     */
+
+    public synchronized static WeatherDB getInstance(Context context){
+        if (weatherDB == null){
+            weatherDB = new WeatherDB(context);
+        }
+
+        return weatherDB;
+    }
+
+    /**
+     * 存储省份信息
+     * @param province
+     */
+    public void saveProvince(Province province){
+        if (province != null){
+            ContentValues values = new ContentValues();
+            values.put("province_name", province.getProvinceName());
+            values.put("province_code", province.getProvinceCode());
+            db.insert("Province", null, values);
+        }
+    }
+
+    /**
+     * 读取省份信息
+     */
+
+    public List<Province> loadProvince (){
+        List<Province> list = new ArrayList<Province>();
+        Cursor cursor = db.query("Province", null, null, null, null, null, null);
+        if (cursor.moveToFirst()){
+            do {
+                Province province = new Province();
+                province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+                list.add(province);
+            }while (cursor.moveToNext());
+        }
+        return list;
+    }
+
+    /**
+     * 存储城市信息
+     */
+
+    public void saveCity(City city){
+        if (city != null){
+            ContentValues values = new ContentValues();
+            values.put("city_name", city.getCityName());
+            values.put("city_code", city.getCityCode());
+            values.put("province_id",city.getProvinceId() );
+            db.insert("City",null,values);
+        }
+    }
+
+    public List<City> loadcity (int provinceId){
+        List<City> list = new ArrayList<City>();
+        Cursor cursor = db.query("City", null, "province_id = ?", new String[]{String.valueOf(provinceId)}, null, null, null);
+        if (cursor.moveToFirst()){
+            do {
+                City city = new City();
+                city.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+                city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+                city.setProvinceId(provinceId);
+                list.add(city);
+            }while (cursor.moveToNext());
+        }
+        return list;
+    }
+
+    /**
+     * 存储县区信息
+     */
+
+    public void saveCounty(County county){
+        if (county != null){
+            ContentValues values = new ContentValues();
+            values.put("county_name", county.getCountyName());
+            values.put("county_code", county.getCountyCode());
+            values.put("city_id", county.getCityId());
+            db.insert("County", null, values);
+        }
+    }
+
+    public List<County> loadCounty(int cityId ){
+        List<County> list = new ArrayList<County>();
+        Cursor cursor = db.query("County", null, "city_id = ?",
+                new String[] {String.valueOf(cityId)}, null, null, null);
+        if (cursor.moveToFirst()){}
+        do {
+            County county = new County();
+            county.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
+            county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
+            county.setCityId(cityId);
+            list.add(county);
+        }while (cursor.moveToNext());
+
+        return list;
+    }
+
+}
---
+++ app/src/main/java/com/example/lenovo/weather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.lenovo.weather.model;
+
+/**
+ * Created by lenovo on 2016/5/13.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
